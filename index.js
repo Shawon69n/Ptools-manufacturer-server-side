5682cade3fafac8f47ef1927d3c21e65ad31597e
ShawonsGit <s.shawon0000@gmail.com>
2022-05-26 22:44:50 +0600

    admin route impliment

--- index.js
+++ index.js
@@ -60,6 +60,32 @@ async function run(){
              res.send(result);
          })
 
+
+        //  admin section 
+
+        app.put('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+              $set: { role: 'admin' },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+    
+        })
+
+
+        app.get('/admin/:email',async(req,res) =>{
+            const email = req.params.email;
+            const user = await usersCollection.findOne({email : email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin : isAdmin})
+            
+          })
+  
+  
+        
+
     }
 
     finally{
